baa132ce3acc0edb2d228894348dd8566f494fb4
Tommy Cervenka <drumsmylife@Tommys-MacBook-Pro.local>
2019-02-15 19:44:55 -0600

    fixed keys.js file

--- liri-node/keys.js
+++ liri-node/keys.js
@@ -1,14 +1,13 @@
-```js
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.0f88e8953ef349a88aea51469e571d68,
-  secret: process.env.e5049a722253461a9f908097a7fc057a
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
-```
 
-//bands in town api
-https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
 
-// omdb api key
-http://www.omdbapi.com/?i=tt3896198&apikey=a9b1a481
+// //bands in town api
+// https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+// // omdb api key
+// http://www.omdbapi.com/?i=tt3896198&apikey=a9b1a481
--- liri-node/liri.js
+++ liri-node/liri.js
@@ -4,4 +4,4 @@ var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
 
-node liri.js spotify-this-song <artist/band name here>`
+
