c0cf46f995bb633f75ac8f07f25bfb335d5c6e6d
Tommy Cervenka <drumsmylife@Tommys-MacBook-Pro.local>
2019-02-15 20:08:16 -0600

    added variables to liri.js

--- liri-node/liri.js
+++ liri-node/liri.js
@@ -1,7 +1,21 @@
+//requires .env
 require("dotenv").config();
 
+//requires key.js file
 var keys = require("./keys.js");
 
+//require moment package
+var moment = require("moment");
+
+//require file systems
+var fs = require("fs");
+
+//require axios package
+var axios = require("axios");
+
+//start spotify
 var spotify = new Spotify(keys.spotify);
+
+
 
 
